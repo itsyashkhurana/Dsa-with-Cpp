dbed799cd55f7ae296c170c33ca49724d485b1a3
Yash khurana <96585577+Yashkhhurana@users.noreply.github.com>
2024-07-21 11:31:35 +0000

    List - code

--- 5_Standard Template Library/list.cpp
+++ 5_Standard Template Library/list.cpp
@@ -1,5 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
 list<int> li;
 li.push_back(30);
 li.push_back(50);
 li.push_front(20);
-cout<<li.front();
+cout<<li.front()<<"\n";
+cout<<li.back()<< "\n";
+cout<<"hello"<<"\n ";
+}
