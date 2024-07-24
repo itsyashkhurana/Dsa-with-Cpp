3abd03cd641e5b59280f213d2f21b974e728886e
Yash khurana <96585577+Yashkhhurana@users.noreply.github.com>
2024-07-24 14:21:47 +0000

    selection sort

--- 9_Sorting/sorting.cpp
+++ 9_Sorting/sorting.cpp
@@ -1,35 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-void selection_sort(int arr[], int n) {
-  // selection sort
-  for (int i = 0; i < n - 1; i++) {
-    int mini = i;
-    for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[mini]) {
-        mini = j;
-      }
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Find the minimum element in unsorted array
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+
+        // Swap the found minimum element with the first element    
+        swap(arr[min_idx], arr[i]);
     }
-    int temp = arr[mini];
-    arr[mini] = arr[i];
-    arr[i] = temp;
-  }
-
-  cout << "After selection sort: " << "\n";
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-
 }
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
 int main() {
-  int arr[] = {13,46,24,52,20,9};
-  int n = sizeof(arr) / sizeof(arr[0]);
-   cout << "Before selection sort: " << "\n";
-   for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-  selection_sort(arr, n);
-  return 0;
+    int arr[] = {64, 25, 12, 22, 11, 90 , 70};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+ /* In this example, sizeof(arr) is the total size of the array, which is 24 bytes 
+  (assuming int is 4 bytes on your system). sizeof(arr[0]) is the size of an integer,
+   which is 4 bytes. Therefore, n will be calculated as 24 bytes / 4 bytes = 6.     */
+
+    selectionSort(arr, n);
+
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
 }
