93841051ed6aef4883ccaa3f383135a49f8ca19a
Yash khurana <96585577+Yashkhhurana@users.noreply.github.com>
2024-08-05 09:12:20 +0000

    Fibonacci Number

---
+++ 10_Arrays/Fibonacci Number.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int arr[1000];
+    int n ;
+    cin>>n;
+
+arr[0]=0;
+arr[1]=1;
+    for(int i=2;i<=n-1;i++){
+    arr[i]=arr[i-1]+arr[i-2];
+    }
+	cout<<arr[n-1]<<"\n\n";
+}
