374bf779aff601b097fc5aada260498cce18440a
Yash khurana <96585577+Yashkhhurana@users.noreply.github.com>
2024-08-10 11:43:29 -0400

    Update Missing number.cpp

--- 10_Arrays/Missing number.cpp
+++ 10_Arrays/Missing number.cpp
@@ -1,15 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
+int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum=0;
 
-int main() {
-	
-	  int arr[6]={1,2,3,5,6};
-    int sum=0;
-
-    for (int i = 0; i < 5; i++) {
-        sum+= arr[i];             // sum of all element in an array 
+         for (int i = 0; i < n; i++) {
+              sum+= nums[i];             // sum of all element in an array 
+         }
+         
+     int ans = n*(n+1)/2;               // sum of n number
+        return ans-sum;
     }
-int ans = 6*(6+1)/2;       // sum of n number
-  cout<< ans-sum<< "\n\n";
-	
-}
