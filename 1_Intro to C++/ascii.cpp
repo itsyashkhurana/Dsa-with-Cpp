9bc0052ad3a72e4dfa32083f16c2defa7092dbc0
Yash Khurana <96585577+itsyashkhurana@users.noreply.github.com>
2025-05-05 10:30:19 +0000

    update

--- 1_Intro to C++/ascii.cpp
+++ 1_Intro to C++/ascii.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
       using namespace std;
       int main(){
      char x,y;
      cin>>x>>y;
-     cout<<(int(x-y));
+     cout<<(int(x-y)); 
    }
-   
